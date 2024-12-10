d6c330da17166af081f588cd81c9355961720e86
reward-rabieth <rewardrabieth06@gmail.com>
2024-12-10 21:20:15 +0300

    first-commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+use clap::{Parser, Subcommand};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+/// Doc comment
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Doc comment
+    Init,
+}
+
+fn main() {
+    let args = Args::parse();
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref:refs/heads/main\n").unwrap();
+            println!("intialized git-directory");
+        }
+    }
+}
